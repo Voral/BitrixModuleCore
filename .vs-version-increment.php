7a7c09bbafc19ea5a93bb0c7223497778736be7c
Alexander Vorobyev <voral.adm@gmail.com>
2025-05-07 00:26:12 +0300

    chore: Настройки инкремента версий

--- .vs-version-increment.php
+++ .vs-version-increment.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Vasoft\VersionIncrement\Config;
 
 return (new Config())
+    ->setSection('fix', 'Исправления')
+    ->setSection('feat', 'Новый функционал')
+    ->setSection('build', 'Build system', hidden: true)
     ->setSection('test', 'Tests', hidden: true)
     ->setSection('docs', 'Documentation', hidden: true)
     ->setSection('chore', 'Other changes', hidden: true)
